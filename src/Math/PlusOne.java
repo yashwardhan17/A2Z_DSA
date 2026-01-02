f9d93db95e75c9f61860bfbf094f72fbaea65a6f
yashwardhan17 <yashwardhan.1717@gmail.com>
2026-01-02 23:37:19 +0530

    ADD : PlusOne (Leetcode 66)

---
+++ src/Math/PlusOne.java
@@ -0,0 +1,23 @@
+package Math;
+
+import java.util.Arrays;
+
+public class PlusOne {
+    public static void main(String[] args) {
+        int[] arr = {9,9};
+        System.out.println(Arrays.toString(plusOne(arr)));
+    }
+
+    static int[] plusOne(int[] arr){
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] < 9) {
+                arr[i]++; //arr[i] = arr[i] + 1;
+                return arr;
+            }
+            arr[i] = 0;
+        }
+        int[] result = new int[arr.length + 1];
+        result[0] = 1;
+        return result;
+    }
+}
